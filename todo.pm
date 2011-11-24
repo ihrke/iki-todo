378e197a23dd243ccad1922428adfe9f3d460e0a
Matthias Ihrke <ihrke@nld.ds.mpg.de>
2011-11-24 16:37:55 +0100

    I think I got it now

--- todo.pm
+++ todo.pm
@@ -1,4 +1,13 @@
 #!/usr/bin/perl
+#
+# Everytime a [[!todolist]] directive is found, add_depends() adds
+#  dependencies such that this page is build again, whenever those
+#  pages are modified.  I just pass * to add_depends for now, so every
+#  page that has a [[!todolist]] directive is rebuilt whenever any
+#  page changes.
+#
+#
+
 package IkiWiki::Plugin::todo;
 use warnings;
 use strict;
@@ -12,8 +21,24 @@ my %todos; # holds page=>[ todo1, todo2, ... ]
 
 sub import {
 	 hook(type => "scan", id => "todo", call => \&scan_for_todos);
+	 hook(type => "needsbuild", id => "todo", call => \&todo_needsbuild);
+
     hook(type => "getsetup", id => "todo", call => \&getsetup);
     hook(type => "preprocess", id => "todo", call => \&todo_preprocess);
+    hook(type => "preprocess", id => "todolist", call => \&todolist_preprocess);
+}
+
+sub todo_needsbuild {
+	 # careful with sequence:
+	 #  * first the todo-pages must be built and the hash be constructed
+	 #  * then the todo-list can be compiled
+	 my $listref=shift;
+	 debug(  Dumper( @{ $listref } ) );
+	 foreach my $f (@{ $listref }){
+		  my $c = readfile($config{'srcdir'}."/".$f);
+	 }
+
+	 
 }
 
 
@@ -54,11 +79,32 @@ sub todo_preprocess {
 				$todos{$page}=[];
 		  }
 		  push( @{ $todos{$page} }, $text );
+		  unless( exists $wikistate{'todo'}{$page} ){
+				$wikistate{'todo'}{$page}=[];
+		  }
+		  push( @{ $wikistate{'todo'}{$page} }, $text );
+
 		  $output.="<div id='todo_open'>".$text."</div>";
 	 } elsif ( $type eq 'done' ){
 		  $text=$params{text} or die "need text for a todo item";
 		  $output.="<div id='todo_done'>".$text."</div>";
-	 } elsif( $type eq 'list' ){
+	 } 
+
+	 return $output;
+}
+
+sub todolist_preprocess {
+    my %params=@_;
+	 my $page=$params{destpage}; # this is the current page
+	 my $type="list";
+	 my $text="";
+	 my $output="";
+
+	 if( exists $params{type} ){
+		  $type=$params{type};
+	 }
+
+	 if( $type eq 'list' ){
 		  foreach my $key ( keys %todos ) {
 				foreach my $el (@{ $todos{$key} }) {
 					 $output .= "* [[".$el."|".$key."]]\n";
@@ -66,6 +112,8 @@ sub todo_preprocess {
 		  }
 	 }
 
+	 debug(">> todo: adding dependency");
+	 add_depends($params{page}, '*');
 
 	 return $output;
 }
